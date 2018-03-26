85daf7368b0976cd44c08250da26b1284fc4a6ed
Ville Viklund <ville.viklund@morgan.fi>
2018-03-26 21:00:46 +0300

    Eslint and editorconfig

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+      <router-view></router-view>
     </div>
 </template>
 
 <script>
-  export default {
-    name: 'app'
-  }
+export default {
+  name: "app"
+};
 </script>
 
 <style>
-    #app {
-        margin: 2rem auto;
-        max-width: 1080px;
-        color: #2c3e50;
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-    }
+#app {
+  margin: 2rem auto;
+  max-width: 1080px;
+  color: #2c3e50;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 </style>
