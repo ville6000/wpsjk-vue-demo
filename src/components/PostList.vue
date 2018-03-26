c6589e48806fc405c7f5e4b5793e2c151a9e4109
Ville Viklund <ville.viklund@morgan.fi>
2018-03-26 21:14:39 +0300

    Add PostList component

---
+++ src/components/PostList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="post-list">
+    <h1>Post List</h1>
+
+    <ul>
+      <li
+        v-for="post in posts"
+        :key="post.id"
+        v-html="post.title.rendered" />
+    </ul>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'PostList',
+    data () {
+      return {
+        posts: []
+      }
+    },
+    created () {
+      this.fetchPosts()
+    },
+    methods: {
+      fetchPosts () {
+        axios.get(process.env.VUE_APP_REST_URL).then((response) => {
+          this.posts = response.data
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import PostList from '@/components/PostList'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      name: 'PostList',
+      path: '/',
+      component: PostList
+    }
+  ]
 })
